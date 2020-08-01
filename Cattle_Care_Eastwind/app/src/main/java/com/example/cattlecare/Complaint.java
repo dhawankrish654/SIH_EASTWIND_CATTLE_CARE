602c64e903784c8360870acc666b23bed70ee793
anshshanker <shankeransh1999@gmail.com>
2020-08-01 17:53:41 +0530

    Add files via upload

---
+++ Cattle_Care_Eastwind/app/src/main/java/com/example/cattlecare/Complaint.java
@@ -0,0 +1,55 @@
+package com.example.finalsih;
+
+import android.graphics.Bitmap;
+
+public class Complaint {
+
+    private String ID;
+    private String Address;
+    private String Status;
+
+
+
+
+    public Complaint() {
+    }
+
+    public Complaint(String id, String address, String status) {
+        ID = id;
+        Address = address;
+        Status = status;
+
+
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+
+
+
+
+
+
+}
